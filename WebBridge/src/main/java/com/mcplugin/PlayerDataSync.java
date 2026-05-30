4a62a7a77c3e5774d0e9c4a09bc5c334db3140a9
MolRain <molrain1314@gmail.com>
2026-05-30 17:01:58 +0800

    Delete WebBridge directory

--- WebBridge/src/main/java/com/mcplugin/PlayerDataSync.java
+++
@@ -1,8 +0,0 @@
-package com.mcplugin;
-
-import org.bukkit.entity.Player;
-
-public class PlayerDataSync {
-    public PlayerDataSync(WebBridgePlugin p, DatabaseManager d) {}
-    public void savePlayerData(Player player) {}
-}
